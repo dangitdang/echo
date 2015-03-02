053e34ac91d07bccd52e0b80dd65fa5ec8135955
Dang Pham <dangpham@mit.edu>
2015-03-01 22:26:40 -0500

    Init commit
    Initial commit for the application

---
+++ Echo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Echo
+//
+//  Created by Dang Pham on 3/1/15.
+//  Copyright (c) 2015 Quartet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
