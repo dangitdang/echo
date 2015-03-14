add5ec4e5719354362f72b355dc5e2494e1dc6ad
aivanov93 <aivanov@mit.edu>
2015-03-14 19:54:54 -0400

    added and set up Parse

--- Echo/AppDelegate.swift
+++ Echo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.setApplicationId("MUJzfsX8Y7z6xm4PsXrwyr3GTCRHPnJmVOF4lhDf", clientKey: "ywrNxXXEcg2gUnbSgZJwozopJfWRjyGp1fdUONfk")
         return true
     }
 
---
+++ Echo/ParseImporter.m
@@ -0,0 +1,9 @@
+//
+//  ParseImporter.m
+//  Echo
+//
+//  Created by aivanov on 14.03.15.
+//  Copyright (c) 2015 Quartet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
