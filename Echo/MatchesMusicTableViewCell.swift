fed964a57aed252e24ba279da3f9933085e8339f
Harini Suresh <hsuresh@Harinis-MacBook-Air.local>
2015-04-05 19:51:24 -0400

    "redid match page"

---
+++ Echo/MatchesMusicTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MatchesMusicTableViewCell.swift
+//  Echo
+//
+//  Created by Harini Suresh on 4/4/15.
+//  Copyright (c) 2015 Quartet. All rights reserved.
+//
+
+import UIKit
+
+class MatchesMusicTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var artistNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
