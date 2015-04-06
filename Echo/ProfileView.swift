099f24523a4586d98d9bd1abf50cb38a2b2d757a
Hansa Srinivasan <hansa@mit.edu>
2015-04-05 23:39:16 -0400

    Profile Stuff

--- Echo/ProfileView.swift
+++ Echo/ProfileView.swift
@@ -8,20 +8,50 @@
 
 import UIKit
 
-class ProfileView: ViewControllerWNav{
+class ProfileView: ViewControllerWNav, UITextFieldDelegate {
     
     @IBOutlet weak var profPic: UIImageView!
-    
     @IBOutlet weak var name: UILabel!
-    
     @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var checkbox1: UIButton!
+    @IBOutlet weak var checkbox2: UIButton!
+    @IBOutlet weak var checkbox3: UIButton!
+    
+    @IBAction func checkbox1(sender: AnyObject) {
+        checkbox1.selected = !checkbox1.selected
+    }
+    
+    @IBAction func checkbox2(sender: AnyObject) {
+        checkbox2.selected = !checkbox2.selected
+    }
+    
+    @IBAction func checkbox3(sender: AnyObject) {
+        checkbox3.selected = !checkbox3.selected
+    }
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        checkbox1.setImage(UIImage(named: "CheckedCheckbox"), forState: UIControlState.Selected);
+        checkbox2.setImage(UIImage(named: "CheckedCheckbox"), forState: UIControlState.Selected);
+        checkbox3.setImage(UIImage(named: "CheckedCheckbox"), forState: UIControlState.Selected);
+
         
         let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
         let user = appDelegate.user as User!
+        
+        if (user.country == nil) {
+            locationField.placeholder = "Location"
+        } else {
+            locationField.placeholder = user.country
+        }
+        
+        if (user.birthdate == nil) {
+            ageField.placeholder = "Age"
+        } else {
+            ageField.placeholder = user.birthdate
+        }
         
         name.text = user.displayName
         
@@ -31,6 +61,28 @@ class ProfileView: ViewControllerWNav{
             profPic.image = UIImage(data: data!)
         }
         
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        //let user = appDelegate.user as User!
+        
+        if (textField == locationField) {
+            println("LOCATIONFIELD")
+            println(textField.text)
+            locationField.placeholder = textField.text
+            appDelegate.user?.country = textField.text
+            println(appDelegate.user?.country)
+        }
+        if (textField == ageField) {
+            println("AGEFIELD")
+            println(textField.text)
+            ageField.placeholder = textField.text
+            appDelegate.user?.birthdate = textField.text
+            println(appDelegate.user?.birthdate)
+        }
+        return false
     }
     
 }
