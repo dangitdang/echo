b18fab209542ef276e162d281f2f25779f77e95c
aivanov93 <aivanov@mit.edu>
2015-04-07 14:04:27 -0400

    dont remember

--- Echo/User.swift
+++ Echo/User.swift
@@ -154,6 +154,7 @@ class User: Hashable {
         for score in self.matches.keys.array {
             self.matches[score]!.removeObject(user)
         }
+        println(self.matches)
     }
 
     func pullMatches() -> Void {
