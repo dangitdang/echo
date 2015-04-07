7afde0e83ab33cd61d53a4915c20db836ca823e8
Dang Pham <dangpham@mit.edu>
2015-04-06 23:31:08 -0400

    Start working on dialogs

---
+++ Echo/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Echo
+//
+//  Created by Dang Pham on 4/6/15.
+//  Copyright (c) 2015 Quartet. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class MessageCell : UITableViewCell {
+    
+    @IBOutlet weak var userMessage: UILabel!
+    
+    @IBOutlet weak var otherMessage: UILabel!
+    
+    @IBOutlet weak var userPic: UIImageView!
+    @IBOutlet weak var otherPic: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
