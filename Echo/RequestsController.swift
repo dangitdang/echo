c645ad1d176be88f6b390610086e85ab11da821c
Hansa Srinivasan <hansa@mit.edu>
2015-04-06 23:17:49 -0400

    yeah

--- Echo/RequestsController.swift
+++ Echo/RequestsController.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 
+class RequestsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var personPic: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
+
 class RequestsController: ViewControllerWNav, UITableViewDataSource, UITableViewDelegate {
     var userList: [User] = []
     
